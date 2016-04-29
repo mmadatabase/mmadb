8ec5069559c3abf00808ca0737512ef6018cae2f
Darren Hamilton <darren.s.hamilton@gmail.com>
2016-04-29 23:58:28 +0100

    Created Fighter Table.

---
+++ database/migrations/2016_04_29_225653_create_fighters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFightersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fighters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('nickname');
+            $table->enum('nicknamePosition', ['front', 'middle', 'end']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fighters');
+    }
+}
