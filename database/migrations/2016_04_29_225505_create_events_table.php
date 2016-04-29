b7b5879bb9ae0c0c59bf864ae618f8415838111c
Darren Hamilton <darren.s.hamilton@gmail.com>
2016-04-29 23:56:33 +0100

    Added Events Table.

---
+++ database/migrations/2016_04_29_225505_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('promotionID');
+            $table->date('date');
+            $table->string('location');
+            $table->string('venue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
