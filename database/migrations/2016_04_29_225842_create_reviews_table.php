895f7e44282333d3873a4659844f1f7b8bc99890
Darren Hamilton <darren.s.hamilton@gmail.com>
2016-04-29 23:59:31 +0100

    Create Reviews Table.

---
+++ database/migrations/2016_04_29_225842_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fightID');
+            $table->integer('userID');
+            $table->integer('rating');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reviews');
+    }
+}
